c2bef21caf079c200397b77fb88f8b4a994d791f
maquina_oracle <fernando.spinelli@fatec.sp.gov.br>
2022-11-09 02:29:38 +0000

    teste - versão populando a tabela no BD

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
-const axios = require ("axios")
-const SteamAPI = require("steamapi");
-const steam = new SteamAPI("steam token");
+import {urlGetListaDeIdsJogos, urlGetDetalhesJogo, urlPostBd} from "./urls/urls"
 
-const urlGetListaDeIdsJogos = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=1640848EDE04C9DDF3967D8655B2C265&format=json"
-const urlGetDetalhesJogo = "https://store.steampowered.com/api/appdetails?appids="
-const urlPostBd = "https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/tb_jogo/"
+const axios = require ("axios");
+const express = require ("express")
+const { parse } = require("himalaya");
+const regex = /[^0-9a-zA-Z() " : , { } @ . / -]/gi
+const extraiRequisitos = (requisito)=>{
+    const reqHtml = requisito
+    const reqJson = parse(reqHtml)
+    let dadosReq, cpu, ram, armazenamento, gpu
+  
+    // for (let i = 0; i < 10; i++) {
+    //   console.log('teste');
+    //   if (reqJson?.[i]?.children && reqJson?.[i]?.children?.[i+2]) {
+    //     console.log('teste')
+    //     dadosReq = reqJson?.[i]?.children
+    //   }
+    // }
+  
+    if (reqJson?.[0]?.children && reqJson?.[0]?.children?.[1]) {
+      dadosReq = reqJson?.[0]?.children 
+    }
+    else if (reqJson?.[1]?.children && reqJson[1].children?.[1]) {
+      dadosReq = reqJson?.[1]?.children
+    }
+    else if (reqJson?.[2]?.children && reqJson[2].children?.[1]) {
+      // console.log(reqJson[2].children?.[1]);
+      dadosReq = reqJson?.[2]?.children
+    }
+    else if (reqJson?.[3]?.children && reqJson[3].children?.[1]) {
+      dadosReq = reqJson?.[3]?.children
+    }
+    else {
+      dadosReq = reqJson?.[2]?.children
+    }
+    
+    for (const obj of dadosReq) {
+      let categoria
+      let peca
+      if(obj?.children?.[0]?.children?.[0]){
+        categoria = obj.children[0].children[0].content
+      }
+      else if(obj?.children?.[0]){
+        categoria = obj.children[0].content
+      }
+      else{
+        categoria = ''
+      }
+      if (obj?.children?.[1]?.content) {
+        peca = obj?.children[1]?.content
+        switch (categoria) {
+          case 'Processor:' || 'Processador':
+            cpu = peca.replace(regex, '');
+            break;
+          case 'Memory:' || 'Memória':
+            ram = peca.replace(regex, '');
+            break;
+          case 'Graphics:' || 'Placa de vídeo:'||  'Video Card:':
+            gpu = peca.replace(regex, '');
+            break;
+          case 'Storage:' || 'Armazenamento' || 'Hard Disk Space':
+            armazenamento = peca.replace(regex, '');
+            break;
+        }
+      }
+    }
+    // console.log(resultado);
+    const requisitos = `{"Armazenamento": "${armazenamento}","Cpu": "${cpu}","Gpu": "${gpu}","Ram": "${ram}"}`
+    // console.log(requisitos)
+    return requisitos;
+  
+}
+
+const app = express();
+app.listen(80)
+
+app.get("/teste", (req, res) => {
+    res.send("Oi sou seu servidor :)")
+})
 
 const funcao = async() =>{
     let contador = 0
@@ -15,35 +87,45 @@ const funcao = async() =>{
         console.log("Falhei na requisição lista". e)
     }
 
-    for(idJogo of listaIdsENomesJogos.data.applist.apps){
-        if(contador <= 199){
+    for(const item of listaIdsENomesJogos.data.applist.apps){
+        if(contador <= 190){
             console.log("vou chamar os detalhes do jogo, contador: " + contador)
-            let jogo = undefined
-            try{
-                jogo = await axios.get(urlGetDetalhesJogo + idJogo?.appid) 
-            }catch(e){
-                console.log("Falhei na requisição jogo". e)
-            }
-            
-            if(jogo.data?.type === "game"){
-                console.log("achei um do tipo game")
-                let jogoDTO = {
-                    id_jogo_steam: jogo?.steam_appid,
-                    nome: jogo?.name,
-                    imagem: jogo?.header_image,
-                    requisitosMinimos: jogo?.pc_requirements?.minimum,
-                    requisitosRecomendados: jogo?.pc_requirements?.recommended,
-                    preco: jogo?.price_overview?.final_formatted
-                }
-                console.log(jogoDTO.nome)
-                try{
-                    await axios.post(urlPostBd, jogoDTO)
-                }
-                catch(e){
-                    console.log("Falhei no post BD", e)
-                }
+            const idJogo = item.appid
+            const objetoJogo = await axios.get(urlGetDetalhesJogo + idJogo) 
+            const detalheJogo = objetoJogo?.data[idJogo]?.data
+            if (objetoJogo?.data?.[idJogo]?.success === true){
+
                 
-            }  
+                if(detalheJogo?.type === "game"){
+                    console.log("Nome:", detalheJogo?.name);
+                    console.log("achei um jogo");
+                    let jogo = {
+                        id_jogo_steam: detalheJogo?.steam_appid,
+                        nome: detalheJogo?.name.replace(regex, ''),
+                        imagem: detalheJogo?.header_image,
+                        preco: detalheJogo?.price_overview?.final_formatted,
+                    }
+
+                    if (detalheJogo?.pc_requirements?.minimum) {
+                        jogo.requisitosminimos = `${extraiRequisitos(detalheJogo?.pc_requirements?.minimum)}`
+                    }
+        
+                    if (detalheJogo?.pc_requirements?.recommended) {
+                        jogo.requisitosrecomendados = `${extraiRequisitos(detalheJogo?.pc_requirements?.recommended)}`
+                    }
+
+                    console.log(jogo.nome)
+
+                    try{
+                        await axios.post(urlPostBd, jogo)
+                        console.log("Inseriu")
+                    }
+                    catch(e){
+                        console.log("Falhei no post BD", e)
+                    }
+                    
+                }  
+            }
             contador ++
         }
         else{
@@ -53,4 +135,5 @@ const funcao = async() =>{
         }                      
     }
 }
-funcao()
+// funcao()
+
---
+++ urls/urls.js
@@ -0,0 +1,3 @@
+export const urlGetListaDeIdsJogos = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=1640848EDE04C9DDF3967D8655B2C265&format=json"
+export const urlGetDetalhesJogo = "https://store.steampowered.com/api/appdetails?appids="
+export const urlPostBd = "https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/jogo_tb/"
