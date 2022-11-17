6c84906f576792cf9e9ed22cf372aaae4024324f
Fernando Spinelli <fernando.b.spinelli@gmail.com>
2022-11-17 00:39:36 -0300

    teste - quebrado, apenas desenvolvimento

---
+++ src/models/PlacaDeVideo.js
@@ -0,0 +1,43 @@
+import urls from '../urls/urls.js'
+import axios from 'axios';
+const regex = new RegExp('([()])', 'ig');
+
+console.log(urls.g);
+
+// axios.get(urlGetAllPlacas).then((retorno) => {
+//     const nomesDePlacas = retorno.data.items; //.map(placa => placa.gpu);
+//     let requisito = 'Radeon RX 580 series 8GB'.replace(regex,'');
+//     let resultado = pesquisaUmaPlacaNaLista(requisito,nomesDePlacas);
+//     console.log(resultado);
+// })
+ 
+function calculaQuantidadePalavrasEmComum(frase1, frase2){
+    let palavrasFrase1 = frase1.toUpperCase().split(' ');
+    let palavrasFrase2 = frase2.toUpperCase().split(' ');
+    let quantidadePalavrasEmComum = 0;
+
+    palavrasFrase1.forEach((palavraFraseBase) => {
+        palavrasFrase2.forEach((palavraFraseAlvo) => {
+            if (palavraFraseBase === palavraFraseAlvo){
+                quantidadePalavrasEmComum ++;
+            }
+        })
+    })
+    return quantidadePalavrasEmComum;
+}
+
+function pesquisaUmaPlacaNaLista(stringPlacaAlvo, listaPlacas){
+    let melhorCorrespondencia = calculaQuantidadePalavrasEmComum(stringPlacaAlvo, listaPlacas[0]);
+    let indexMelhorCorrespondencia;
+    
+    listaPlacas.forEach((nomePlacaDaLista, index) => {
+        let palavrasEmComum = calculaQuantidadePalavrasEmComum(nomePlacaDaLista,stringPlacaAlvo);
+        if(palavrasEmComum > melhorCorrespondencia){
+            indexMelhorCorrespondencia = index;
+            melhorCorrespondencia = palavrasEmComum;
+        }
+    })
+    return listaPlacas[indexMelhorCorrespondencia];
+}
+
+export default {calculaQuantidadePalavrasEmComum, pesquisaUmaPlacaNaLista}
