8c4f7d28b1e36c89ade5da71de47c5190c2ff3bc
Fernando Spinelli <fernando.b.spinelli@gmail.com>
2022-11-21 12:36:39 -0300

    teste - função calcula placa por ano, ainda não testada

--- src/models/PlacaDeVideo.js
+++ src/models/PlacaDeVideo.js
@@ -7,18 +7,19 @@ const espacosEparenteses = new RegExp('([()])|\s', 'ig');
 let listaPlacasBD;
 
 async function obterListaPlacasBD(){
-    await axios.get(urls.getAllPlacasDeVideo).then((retorno) => {
-        listaPlacasBD = retorno.data.items.map(placa => {
-            placa.gpu = placa.gpu.replace(parenteses,'');
-            return placa;
+    if(listaPlacasBD === undefined){
+        await axios.get(urls.getAllPlacasDeVideo).then((retorno) => {
+            listaPlacasBD = retorno.data.items.map(placa => {
+                placa.gpu = placa.gpu.replace(parenteses,'');
+                return placa;
+            })
         })
-    })
+    }
+    return listaPlacasBD;
 }
 
 async function pesquisaUmaPlacaNaListaBD(stringPlacaRequisito){
-    if(listaPlacasBD === undefined){
-        await obterListaPlacasBD();
-    }
+    await obterListaPlacasBD();
     stringPlacaRequisito = stringPlacaRequisito.replace(espacosEparenteses,'');
     let melhorCorrespondecia = 1;
     let melhoresResultados = [];
@@ -34,6 +35,7 @@ async function pesquisaUmaPlacaNaListaBD(stringPlacaRequisito){
             return placaComAnalise
         }
     })
+    //talvez mudar para sort e retornar sem o if da linha 46
     melhoresResultados = melhoresResultados.reduce((anterior, atual) => {
         if(anterior.resultadoAnalise.quantidadePalavrasErradas < atual.resultadoAnalise.quantidadePalavrasErradas){
             return anterior;
@@ -47,7 +49,30 @@ async function pesquisaUmaPlacaNaListaBD(stringPlacaRequisito){
 
 }
 
-pesquisaUmaPlacaNaListaBD('NVIDIA GeForce GTX 1060 or AMD Radeon RX 580 - DirectX 12.0').then((retorno) => console.log(retorno));
+function calculaMelhorPlacaComCriterioLancamento (listaPlacasRequisitos){
+    const date = new Date();
+    const anoAtual = date.getFullYear();
+    
+    let placasAtendemCriterioAno = listaPlacasBD.filter(placa =>  {
+        placa.data_lancamento >= (anoAtual - 4)
+    })
+    placaMaisForte = listaPlacasRequisitos.reduce( (anterior, atual) => {
+        anterior.gflops < atual.gflops ? atual : anterior;
+    })
+    placasAtendemCriterioAno = placasAtendemCriterioAno.sort((anterior, atual) => {
+        if (anterior.gflops > atual.gflops ) return 1;
+        if (anterior.gflops < atual.gflops ) return -1;
+        return 0;
+    })
+    let indexResultadoPlaca
+    placasAtendemCriterioAno.forEach((placa, index) => {
+        if(placa.gflops >= placaMaisForte.gflops) indexResultadoPlaca = index;
+    })
+    return placasAtendemCriterioAno[indexResultadoPlaca];
+    
+}
+
+pesquisaUmaPlacaNaListaBD('3060tI').then((retorno) => console.log(retorno));
 
 
 export default {pesquisaUmaPlacaNaListaBD}
