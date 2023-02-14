8cf20cde478f54c8b222deed37b70531cff602bd
fspinelli-travelex <fspinelli@travelexbank.com.br>
2023-02-14 17:31:06 -0300

    teste - alterado para nova url do BD

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -1,10 +1,10 @@
 const urls =  {
-    getAllPlacasDeVideo : 'https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/ranking_gpu/?limit=500',
+    getAllPlacasDeVideo : 'https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/ranking_ordenado/?limit=500',
     apiGoogle : 'https://api.scaleserp.com/search',
-    getSingleUser : 'https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
-    getAllUsers :'https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
-    postUser : 'https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
-    postFavorito : 'https://g4673849dbf8477-qwkkduaklu8amhgz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/favorito/'
+    getSingleUser : 'https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
+    getAllUsers :'https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
+    postUser : 'https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/usuario/',
+    postFavorito : 'https://g4673849dbf8477-kh8pftimtcmp3b10.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/favorito/'
 
 
 }
